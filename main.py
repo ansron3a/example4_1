990e455c11d5c13520e723fd888343a6bfdaf177
andron3a <andron3a@mail.ru>
2022-12-01 23:39:18 +0300

    Поиск сотрудника по ID

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def employees():
         org_list = json.loads(f.read())
     return render_template('employees.html', org=org_list, css_path=url_for('static', filename='css/emp_style.css'))
 
+# Вывод сотрудника по id
+@app.route('/employee/<int:id>')
+def employee(id=None):
+    with open(url_for('static', filename='data/organization.json')[1:], 'rt', encoding='utf-8') as f:
+        org_list = json.loads(f.read())
+    return render_template('employee.html', org=org_list,
+                           css_path=url_for('static', filename='css/emp_style.css'), id=id)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
