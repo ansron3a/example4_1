69bf02e38ee7fe968d80f83ee2d0ca3a5e4bf453
andron3a <andron3a@mail.ru>
2022-12-01 23:16:00 +0300

    Создание файла emp_style.css и employee.html

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def start():
     return render_template('index.html', css_path=url_for('static', filename='css/index_style.css'))
 
+# Вывод всех сотрудников
+@app.route('/employees')
+def employees():
+    with open(url_for('static', filename='data/organization.json')[1:], 'rt', encoding='utf-8') as f:
+        org_list = json.loads(f.read())
+    return render_template('employees.html', org=org_list, css_path=url_for('static', filename='css/emp_style.css'))
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
